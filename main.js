d3166265d5d53b48aebaf2bb828117a6628a0e6a
Eduardo Salermo <duduscotta@gmail.com>
2025-06-02 15:00:56 -0300

    second commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function analizarHumor(texto){
+    const positivo = ['feliz','alegre', 'animado','grato','otimo','bem' ,'contente','calmo']
+    const negativo = ['triste','cansado' ,'chatedo','pessimo','mal','deprimido','ansioso','estressado','raiva']
+
+}
